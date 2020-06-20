1e3a2a4a9d67aa9048de1ade060a701ed2d7a048
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2020-06-20 17:51:35 -0400

    added extra actions and reducers

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -18,3 +18,52 @@ export const deleteDoctor = doctor => {
         payload: doctor
     };
 };
+
+export const registerPatient = doctor => {
+    return {
+        type: 'REGISTER_PATIENT',
+        payload: doctor
+    };
+};
+
+export const editPatient = doctor => {
+    return {
+        type: "EDIT_PATIENT",
+        payload: doctor
+    };
+};
+
+export const deletePatient = doctor => {
+    return{
+        type: "DELETE_PATIENT",
+        payload: doctor
+    };
+};
+
+export const addSymptom = symptomObj => {
+    return{
+        type: "ADD_SYMPTOM",
+        payload: symptomObj
+    };
+};
+
+export const deleteSymptom = symptomObj => {
+    return{
+        type: "ADD_SYMPTOM",
+        payload: symptomObj
+    };
+};
+
+export const addTreatment = treatmentObj => {
+    return{
+        type: "ADD_SYMPTOM",
+        payload: treatmentObj
+    };
+};
+
+export const deleteTreatment = TreatmentObj => {
+    return{
+        type: "ADD_TREATMENT",
+        payload: treatmentObj
+    };
+};
