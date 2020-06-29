cd59ba1cf0cec96f9e3f9241a5b9ec6cc6f938d8
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2020-06-29 19:33:18 -0400

    Updates

---
+++ precache-manifest.0db19105e0c06a2aa0544b104f819bfb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ad8edcac291d63138f0b02d4e02f55",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/index.html"
+  },
+  {
+    "revision": "6d1cb17dae2b054e137e",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/css/main.9c020c33.chunk.css"
+  },
+  {
+    "revision": "766ba0223d51043de5b8",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/js/2.1b4f6a64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/js/2.1b4f6a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1cb17dae2b054e137e",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/js/main.53090462.chunk.js"
+  },
+  {
+    "revision": "5bccd8cbd0e2d97afbb5",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/js/runtime-main.a64af7bb.js"
+  },
+  {
+    "revision": "adcd2b36155a3d93190e5357f8088dd7",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/media/patient-standard-img.adcd2b36.png"
+  },
+  {
+    "revision": "63a07a67c748e59e7511e3c77e1b906f",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/media/standard_doc_img_1.63a07a67.png"
+  },
+  {
+    "revision": "8bae96e2bce21a6c7a1f242146cbd3a3",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/media/symptom_default_image.8bae96e2.jpg"
+  },
+  {
+    "revision": "7ae17b8c7215919cd68d926645c49708",
+    "url": "/Kyrylo-Glamazdin/Hospital-Management-Frontend/static/media/treatment_default_image.7ae17b8c.png"
+  }
+]);
